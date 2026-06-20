e5d08e02025a10a8032dbe6bbff8e2d1b6a692a1
Tiso Alvarez Puccinelli <28565117+tisoap@users.noreply.github.com>
2026-06-20 18:06:15 -0300

    chore: update Storybook configuration to remove MDX stories from the stories array

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,7 @@ import { dirname } from "node:path";
 import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     getAbsolutePath("@storybook/addon-docs"),
     getAbsolutePath("@chromatic-com/storybook"),
