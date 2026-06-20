ff7f52fd7280056c7617bb2d2b4ef44f2d2c16be
Tiso Alvarez Puccinelli <28565117+tisoap@users.noreply.github.com>
2026-06-20 18:03:35 -0300

    chore: replace @vitejs/plugin-react-swc with @vitejs/plugin-react in package.json and vite.config.ts, and update package-lock.json accordingly

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
